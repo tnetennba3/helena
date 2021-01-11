0b07d1742f0d48cf02a93f7586174235443cc784
tnetennba3 <helena_thompson@hotmail.com>
2021-01-11 21:51:48 +0000

    ✨ Improve wording

--- src/pages/blog/i-read-my-first-book-in-japanese/index.tsx
+++ src/pages/blog/i-read-my-first-book-in-japanese/index.tsx
@@ -25,12 +25,12 @@ const BlogPost = () => (
     <h1>I read my first book in Japanese 🎉</h1>
     <Small>Jan 10, 2021 ・ 9 min read</Small>
     <p>
-      Last year, I decided to start seriously studying Japanese. I set myself an
+      Last year, I embarked on a journey to learn Japanese. I set myself an
       objective to read Harry Potter and the Philosopher’s Stone (or
-      ハリー・ポッターと賢者の石) by the end of the year. Two weeks ago, I
-      accomplished this goal. In this post, I describe my language learning
-      journey, approach to reading, and will present some fun stats and graphs I
-      recorded along the way.
+      ハリー・ポッターと賢者の石) by the end of 2020. Two weeks ago, I
+      accomplished this goal. In this post, I describe my journey so far,
+      approach to reading, and will present some fun stats and graphs I recorded
+      along the way.
     </p>
     <p>
       Back in April, I suddenly found myself with a lot more time on my hands.
@@ -51,9 +51,9 @@ const BlogPost = () => (
       My previous attempts to study the language were largely unsuccessful. I
       dabbled a bit with language learning apps and lost interest quite quickly.
       Even though I am fortunate enough to understand a fair amount of Japanese
-      already (thanks, mum) I just didn’t have much experience or confidence in
-      actively studying a new language. My time learning French and German at
-      school taught me that I could rote-learn vocabulary lists, memorise a
+      already (thanks, mum), I just didn’t have much experience or confidence in
+      being able to acquire a new language. My time learning French and German
+      at school taught me that I could rote-learn vocabulary lists, memorise a
       script by heart and get decent exam results. But I didn’t retain any of
       that information a month later let alone years after I left school. The
       lesson I took away from it all was that{" "}
@@ -67,11 +67,10 @@ const BlogPost = () => (
     <p>
       So, my self-directed language learning journey began with{" "}
       <em>learning how to learn</em>. I quickly realised, though it should have
-      been obvious before, that acquiring a second language isn’t anything
+      been obvious before, that picking up a second language isn’t anything
       special. Most sources say that over half of the world’s population is
-      multilingual. Clearly, I didn’t need to be gifted or attuned to languages
-      to pick one up. I just needed time, dedication, and effective learning
-      methods.
+      multilingual. Clearly, I didn’t need to be gifted or attuned to languages.
+      I just needed time, dedication, and effective learning methods.
     </p>
     <Figure
       src={averageLanguagesSpoken}
@@ -109,22 +108,21 @@ const BlogPost = () => (
       Japanese, that sounds like I could actually enjoy myself.
     </p>
     <p>
-      At the start of April, I set my focus on improving my reading ability. I
-      couldn’t read much Japanese at all and I knew that being able to do so
-      would unlock a whole world of material to immerse myself in. But I was
-      starting from pretty much square one. Japanese has three writing systems -
-      hiragana, katakana and kanji. The first two I knew well enough at the
-      time, there are only 46 characters in each. Kanji, on the other hand, is a
-      whole other beast and has thousands of characters with multiple ways to
-      read and understand each of them. I was familiar with about 20 kanji at
-      the time. The average Japanese adult can read <em>over 3000</em>.
+      At the start of April, I set my focus on improving my reading ability.
+      Doing so would unlock a whole world of material to immerse myself in. But
+      I was starting from pretty much square one. Japanese has three writing
+      systems - hiragana, katakana and kanji. The first two I knew well enough
+      at the time, there are only 46 characters in each. Kanji, on the other
+      hand, is a whole other beast and has thousands of characters with multiple
+      ways to read and understand each of them. I was familiar with about 20
+      kanji at the time. The average Japanese adult can read <em>over 3000</em>.
     </p>
     <Figure
       src={kanji}
       alt="Person with a book over in their face"
       caption={
         <>
-          Here's just a thousand of the{" "}
+          Here's just 1200 of the{" "}
           <cite>
             <a
               href="https://upload.wikimedia.org/wikipedia/commons/1/11/3002_Kanji.svg"
@@ -139,7 +137,7 @@ const BlogPost = () => (
     <p>
       So, knowing that I wanted to get better at reading, I just needed a
       direction to work towards. This goal of reading Harry Potter became my
-      lighthouse and gave purpose to all my other Japanese studies.
+      lighthouse and gave purpose to all the other studies I was undertaking.
     </p>
     <p>
       Of course, I didn’t just spend the past 8 months slowly working through
@@ -161,33 +159,32 @@ const BlogPost = () => (
       </cite>{" "}
       techniques. In future, I might write another post going over the resources
       and methods I used in more detail. But for now, I’ll focus on how I
-      tackled reading my first book in Japanese.
+      tackled reading my first book in Japanese. 📖
     </p>
     <h2>Why Harry Potter?</h2>
     <p>
       Learning a new language unlocks a whole new world of rich content to
       explore. It’s the reason a lot of people study a new language in the first
       place. So, you might be wondering why I chose a book that’s originally
-      written in English. A story I’m already very familiar with and won’t be
+      written in English. A story I’m already very familiar with and wouldn’t be
       getting much out of by reading it again in its impure, translated form.
     </p>
     <p>
-      Though I wouldn’t be discovering a new narrative reading Harry Potter, it
-      wasn’t why I was chose it. I simply wanted to get better at Japanese. And
-      it’s much easier to get started with something that you are already
-      familiar with as you have a lot of context to full back on.
+      Discovering a new narrative wasn't the reason I chose Harry Potter. I
+      simply wanted to get better at Japanese. And it’s much easier to get
+      started with something that you are already familiar with.
     </p>
     <p>
-      For example, I might come across the phrase 組み分け帽子. After looking up
-      in the dictionary, I would discover that the first part (組み分け) means
-      “division into groups” and the second (帽子) means “hat”. Without any
-      knowledge of Harry Potter, this is quite a cryptic phrase and could leave
-      you scratching your head for a while. For those of us that know the story,
-      it’s easy to recognise “Oh, it’s the Sorting Hat!” This meant I had no
-      trouble with made-up names and phrases (which there are a lot of in Harry
-      Potter). These nuggets of knowledge also acted as anchors in the story
-      that meant I could guess where I was and verify if I was reading
-      correctly.
+      For starters, you have a lot of context to full back on. I might come
+      across the phrase 組み分け帽子 and upon checking the dictionary, would
+      discover that the first part (組み分け) means “division into groups” and
+      the second (帽子) means “hat”. Without any knowledge of Harry Potter, this
+      is quite a cryptic phrase and could leave you scratching your head for a
+      while. For those of us that know the story, it’s easy to recognise “Oh,
+      it’s the Sorting Hat!” This meant I had no trouble with made-up names and
+      phrases (which there are a lot of in Harry Potter). These nuggets of
+      knowledge also acted as anchors in the story that would help me verify if
+      I was understanding the text correctly.
     </p>
     <Figure
       src={sortingHat}
@@ -195,10 +192,10 @@ const BlogPost = () => (
       caption="Anything but Slytherin, anything but Slytherin."
     />
     <p>
-      Similarly, I just don’t have a great tolerance for ambiguity. I want to
-      make sure I’m understanding every sentence, every detail when I’m taking
+      Likewise, I don’t have a great tolerance for ambiguity. I like to make
+      sure I’m understanding every sentence, every detail when I’m taking
       something in. This sense of unease, that I’m missing something, is hard to
-      be rid of if you’re reading in a language you lack confidence in. Better
+      overcome if you’re reading in a language you're not confident in. Better
       to start with a tale you already know.
     </p>
     <p>
@@ -217,11 +214,11 @@ const BlogPost = () => (
       I decided I would track my progress during my time reading Harry Potter.
       Things I wanted to track included how many kanji and words I was looking
       up as well as how long it was taking me to read each page. I knew I would
-      be slow, much much slower than if I was reading in English so I needed
-      something to motivate me to keep going. Being able to track my improvement
-      over time became that motivator. Knowing that, even if I’m going at a
-      snail’s pace reading this children’s book, I am at least a lot faster than
-      when I started out.
+      be slow, much much slower than if I was reading in English so I needed a
+      way to keep motivated. Having visibility over my improvement over time
+      became that motivator. Knowing that, even if I’m going at a snail’s pace
+      reading this children’s book, I am at least a lot faster than when I
+      started out.
     </p>
     <p>
       How would I track these stats? Whenever I sat down to read Harry Potter, I
@@ -236,9 +233,12 @@ const BlogPost = () => (
       caption="My very ugly spreadsheet."
     />
     <p>
-      The tracking process was fairly manual. I would like to build an app that
-      automates a lot of this for me. But this might just sit atop my list of
-      projects that I never actually start. 🙈
+      It was all fairly manual. At some point I would like to build an app that
+      automates a lot of this for me.{" "}
+      <small>
+        But this might just sit atop my list of projects that I never actually
+        start. 🙈
+      </small>
     </p>
     <h2>Results</h2>
     <h3>How long it took me</h3>
@@ -253,50 +253,48 @@ const BlogPost = () => (
       <li>565 unique kanji / 975 total kanji</li>
     </ul>
     <p>
-      As you can see, it took me a <i>long time</i> to read Harry Potter and the
-      Philosopher's Stone. To put it into perspective, I could have read over
-      ten English novels in that time. But I expected that going into this
-      journey. What I was more concerned about was whether my Japanese reading
-      would improve over that time. And the answer is a definitive <em>yes</em>.
+      As you can see, it took me a <em>long time</em> to read Harry Potter and
+      the Philosopher's Stone. To put it into perspective, I could have read
+      over ten English novels in that time. But I expected that going into this
+      journey. What I was more concerned about was whether I became a better
+      reader over those 62 hours. And the answer was a definitive <em>yes</em>.
     </p>
     <Figure
       src={readingStatsBySession}
       alt="Graph showing my reading stats by session"
     />
     <p>
-      Right at the start, I was spending an hour and a half reading just one
-      page. But I improved quickly and dramatically. My average reading speed
-      reduced to 51 minutes per page by the end of the first chapter. In the
-      final chapter, I was reading at a pace of 11 minutes per page.
+      The first page took me an <em>hour and a half</em> to read. But I improved
+      quickly and dramatically. My average reading speed reduced to 51 minutes
+      per page by the end of the first chapter. In the final chapter, I was
+      reading at a pace of 11 minutes a page.
     </p>
     <Figure
       src={readingStatsByChapter}
       alt="Graph showing my reading stats by chapter"
     />
     <p>
-      The rate of lookups paints a similar picture. The first chapter had me
+      The rate of lookups tells a similar story. The first chapter had me
       looking up an average of 45 words and 14 kanji per page. Whereas, in the
       final chapter, I only needed to look up 13 words and 3 kanji per page. The
-      decreased rate of lookups wasn’t the only explanatory factor in my
-      increased reading speed either. I was also getting better at
-      distinguishing individual words within a sentence and acclimatising myself
-      to the grammar too.
+      decreased rate of lookups wasn’t the only reason my reading speed was
+      increasing either. I was also getting better at distinguishing individual
+      words within a sentence and acclimatising myself to the grammar.
     </p>
     <p>
-      Not only was my Japanese reading ability advancing, to my surprise, my
-      stamina grew as well. I started out reading just half a page at a time.
-      Despite being very motivated (it was what the previous six months were
-      leading up to after all), I found it so draining that I needed to take a
-      break after 30-40 minutes. This then became another skill I developed over
-      time. From chapter ten, I was able to read whole chapters in a single
-      sitting, spending almost three hours doing so.
+      One thing I hadn't predicted was my stamina growth. I started off only
+      being able to read half a page at a time. Despite being very motivated (it
+      was what the previous six months were leading up to after all), I found it
+      draining enough that I needed to take a break after 30-40 minutes. By
+      chapter ten, I could read whole chapters in a single sitting, spending
+      almost three hours doing so.
     </p>
     <p>
       And yet, progress wasn’t always linear. I clearly had some setbacks.
       Notably, in chapter eight, we spend a lot of time with the infamous
-      Severus Snape. I found his style of speech harder to decipher than the
-      other characters. He was also pretty happy to use obscure kanji whenever
-      he could.
+      Severus Snape. His style of speech harder was a lot harder to decipher
+      than the other characters, not least because of his liberal use of obscure
+      kanji.
     </p>
     <Figure
       src={severusSnape}
@@ -309,13 +307,13 @@ const BlogPost = () => (
       is going to fluctuate. And yet, it’s easy to lose sight of that in the
       moment. You might read a difficult text and think you’re not making any
       headway. But if you take a step back to view your journey over a wider
-      time frame, it’s easy to see that you really are making progress. And
-      that’s where tracking my stats helped me to gain that perspective. I could
-      recognise that chapter eight was just a bump in the road in my quest to
-      get better.
+      time frame, it’s easy to see that you really are making progress. Tracking
+      my stats helped me to gain that perspective. I could recognise that
+      chapter eight was just a bump in the road (or spike in the graph) in my
+      quest to get better.
     </p>
     <p>
-      Overall, I am really happy with the progress I made. Not all of it is
+      Overall, I am pretty pleased with the progress I made. Not all of it is
       going to translate to the next book I read (I’ll be surprised if I come
       across as many breeds of owls any time soon). But there should be some
       knowledge that carries over - common words and kanji, familiarity with
@@ -329,8 +327,8 @@ const BlogPost = () => (
     />
     <p>
       This year, my goal is to read <em>two Japanese books a month</em>! I will
-      continue tracking my reading stats as I go along and hope to report back
-      once I’ve read more.
+      continue tracking my reading stats and hope to report back once I’ve read
+      more.
     </p>
   </Layout>
 )
