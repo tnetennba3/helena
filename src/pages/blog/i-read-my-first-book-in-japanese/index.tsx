4307075ab4c79fbe9d3e9b57c19009cde5b94d04
tnetennba3 <helena_thompson@hotmail.com>
2021-01-12 01:12:34 +0000

    ✨ Update blog meta image

--- src/pages/blog/i-read-my-first-book-in-japanese/index.tsx
+++ src/pages/blog/i-read-my-first-book-in-japanese/index.tsx
@@ -6,7 +6,6 @@ import Layout from "../../../components/layout"
 import SEO from "../../../components/seo"
 import Figure from "../../../components/figure-image"
 
-import cover from "./images/cover.png"
 import animalCrossing from "./images/animal-crossing.jpg"
 import averageLanguagesSpoken from "./images/average-languages-spoken.jpg"
 import kanji from "./images/kanji.png"
@@ -27,7 +26,7 @@ const BlogPost = () => (
     <SEO
       title="I read my first book in Japanese 🎉"
       description="My 2020 goal was to read Harry Potter and the Philosopher’s Stone (or ハリー・ポッターと賢者の石) by the end of the year. Two weeks ago, I accomplished this goal."
-      image={cover}
+      image={illustratedBook}
     />
     <h1>I read my first book in Japanese 🎉</h1>
     <Small>Jan 10, 2021 ・ 9 min read</Small>
